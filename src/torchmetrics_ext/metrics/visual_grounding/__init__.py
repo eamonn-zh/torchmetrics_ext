2b9d2a1e0a90a7a4340aece27d1c718216d24f99
Austin Wang <austin.t.wang@gmail.com>
2025-07-17 15:22:08 -0700

    add vigil3d metric and tests; fix multi3drefer tests

--- src/torchmetrics_ext/metrics/visual_grounding/__init__.py
+++ src/torchmetrics_ext/metrics/visual_grounding/__init__.py
@@ -1,5 +1,6 @@
 from .scanrefer import ScanReferMetric
 from .nr3d import Nr3DMetric
 from .multi3drefer import Multi3DReferMetric
+from .vigil3d import ViGiL3DMetric
 
-__all__ = ["ScanReferMetric", "Nr3DMetric", "Multi3DReferMetric"]
+__all__ = ["ScanReferMetric", "Nr3DMetric", "Multi3DReferMetric", "ViGiL3DMetric"]
